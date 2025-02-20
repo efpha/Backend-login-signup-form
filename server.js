4d59ad38793ef1ce80a9063e0ae327d549968d6c
KEFFA <muthurikeffa254@gmail.com>
2025-02-20 18:15:04 +0300

    server can receive data from signin component

--- server.js
+++ server.js
@@ -71,65 +71,15 @@ App.post('/api/signup',async(req, res) => {
     }
 })
 
+//new signin route
+App.post('/api/signin', async(req,res) => {
+    const { email, password } = req.body;
+    res.send(req.body)
+    console.log(req.body);
+})
 
 
 
-
-
-
-
-
-
-
-
-
-// Connect to MySQL Database
-// db.connect((err) => {
-//     if (err) {
-//         console.log("Database connection failed:", err);
-//         return;
-//     } else {
-//         console.log("Connected to MySQL");
-//     }
-// });
-
-// // Password hashing function
-// const hashPassword = async (Password) => {
-//     const saltRounds = 10;
-//     return await bcrypt.hash(Password, saltRounds);
-// };
-
-// Signup route
-// App.post('/api/signup', async (req, res) => {
-//     const { email, username, password } = req.body;
-    
-//     console.log(req.body);
-
-//     if (email == ""|| username == "" || password == "") {
-//         return res.status(400).send({ message: 'Email, username, and password are required!' });
-//     }
-
-    // try {
-
-
-
-
-
-        // const hashedPassword = await hashPassword(password);
-        // const query = 'INSERT INTO sign_up (Email, username, Password) VALUES (?, ?, ?)';
-
-        // db.query(query, [email, username, hashedPassword], (err, results) => {
-        //     if (err) {
-        //         console.log('Error inserting user:', err);
-        //         return res.status(500).send({ message: 'Server error' });
-        //     }
-        //     res.send({ message: 'User signed up successfully' });
-        // });
-    // } catch (error) {
-        // console.error('Error during signup:', error);
-        // res.status(500).send({ message: 'Server error' });
-    // }
-// });
 
 
 
@@ -155,47 +105,47 @@ App.post('/api/signup',async(req, res) => {
 
 
 // Signin route
-App.post('/api/signin', (req, res) => {
-    const { username, Password } = req.body;
+// App.post('/api/signin', (req, res) => {
+//     const { username, Password } = req.body;
 
-    if (!username || !Password) {
-        return res.status(400).send({
-            message: 'Username and Password are required!'
-        });
-    }
+//     if (!username || !Password) {
+//         return res.status(400).send({
+//             message: 'Username and Password are required!'
+//         });
+//     }
 
-    const query = 'SELECT * FROM sign_up WHERE username = ?';
+//     const query = 'SELECT * FROM sign_up WHERE username = ?';
 
-    db.query(query, [username], async (err, results) => {
-        if (err) {
-            console.log("Error querying the database:", err);
-            return res.status(500).send({
-                message: "Server error"
-            });
-        }
+//     db.query(query, [username], async (err, results) => {
+//         if (err) {
+//             console.log("Error querying the database:", err);
+//             return res.status(500).send({
+//                 message: "Server error"
+//             });
+//         }
 
-        if (results.length === 0) {
-            return res.status(401).send({
-                message: "Invalid username or Password"
-            });
-        }
+//         if (results.length === 0) {
+//             return res.status(401).send({
+//                 message: "Invalid username or Password"
+//             });
+//         }
 
-        const user = results[0];
+//         const user = results[0];
 
-        // Compare provided Password with the stored hashed Password
-        const isPasswordValid = await bcrypt.compare(Password, user.Password);
+//         // Compare provided Password with the stored hashed Password
+//         const isPasswordValid = await bcrypt.compare(Password, user.Password);
 
-        if (!isPasswordValid) {
-            return res.status(401).send({
-                message: "Invalid username or Password"
-            });
-        }
+//         if (!isPasswordValid) {
+//             return res.status(401).send({
+//                 message: "Invalid username or Password"
+//             });
+//         }
 
-        res.status(200).send({
-            message: 'User signed in successfully'
-        });
-    });
-});
+//         res.status(200).send({
+//             message: 'User signed in successfully'
+//         });
+//     });
+// });
 
 App.listen(port, () => {
     console.log(`Server running on port ${port}`);
